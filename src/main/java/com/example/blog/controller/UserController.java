e52afb8588bbce343b289ac4509a0e8d585d2014
Junhyuk Park <chazevo2@gmail.com>
2019-08-22 17:07:53 +0900

    add HATEOS dependency

--- src/main/java/com/example/blog/controller/UserController.java
+++ src/main/java/com/example/blog/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.blog.exception.UserNotFoundException;
 import com.example.blog.service.UserService;
 import com.example.blog.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -12,19 +14,30 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 @RestController
 public class UserController {
     @Autowired
     UserService service;
 
     @GetMapping("/users")
-    public List<User> users() {
+    public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable("id") int id) {
-        return service.findUser(id);
+    public Resource<User> retrieveUser(@PathVariable("id") int id) {
+        User user = service.findUser(id);
+        if (user == null) {
+            throw new UserNotFoundException("id : " + id);
+        }
+
+        Resource<User> resource = new Resource<User>(user);
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+        return resource;
     }
 
     @PostMapping("/users")
