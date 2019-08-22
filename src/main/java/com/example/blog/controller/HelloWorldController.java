b59b3e207e67f5275f7040a1690272923739b9f9
Junhyuk Park <chazevo2@gmail.com>
2019-08-22 17:42:17 +0900

    add message internationalized

--- src/main/java/com/example/blog/controller/HelloWorldController.java
+++ src/main/java/com/example/blog/controller/HelloWorldController.java
@@ -1,10 +1,16 @@
 package com.example.blog.controller;
 
 import com.example.blog.vo.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
     @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
     public String helloWorld() {
         return "Hello, World!";
@@ -18,5 +24,11 @@ public class HelloWorldController {
     @GetMapping(value = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable("name") String name) {
         return new HelloWorldBean(String.format("Hello, %s!", name));
+    }
+
+    @GetMapping(value = "/hello-world/internationalized")
+    public String helloWorldInternationalized() {
+        String msg = messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+        return msg;
     }
 }
